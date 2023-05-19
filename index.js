602c843e3de9749f94b20fa7d863a273b2620f39
cedric <cedricj765@gmail.com>
2023-05-19 11:34:53 -0500

    completed lab

--- index.js
+++ index.js
@@ -8,7 +8,67 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ function createEmployeeRecord(array){
+    let testEmployee = {
+      firstName: array[0], 
+      familyName: array[1], 
+      title: array[2], 
+      payPerHour: array[3],
+      timeInEvents: [], 
+      timeOutEvents: [],
+    }
+    //console.log(testEmployee.access())
+  return testEmployee
+   } 
 
+  
+   function createEmployeeRecords(arrays){
+    let employeeRecords = arrays
+    let newArray = employeeRecords.map(createEmployeeRecord)
+    return newArray
+   }
+  
+   function createTimeInEvent(dateStamp){
+    let newArr = dateStamp.split(' ')
+    let newEvent = {
+      type: "TimeIn",
+      hour: parseInt(newArr[1], 10),
+      date: newArr[0]
+    }
+    //console.log(this)
+    this.timeInEvents.push(newEvent)
+    return this
+     
+   }
+  
+  
+   function createTimeOutEvent(dateStamp){
+    let newArr = dateStamp.split(' ')
+    let newEvent = {
+      type: "TimeOut",
+      hour: parseInt(newArr[1], 10),
+      date: newArr[0]
+    }
+    this.timeOutEvents.push(newEvent)
+    return this
+  }
+  
+  function hoursWorkedOnDate(date){
+    let timeInHours = this.timeInEvents.find(days => days.date === date)
+    let timeOutHours = this.timeOutEvents.find(days => days.date === date)
+    //console.log(timeInHours)
+     return (timeOutHours.hour - timeInHours.hour) / 100
+    //console.log(timeInHours)
+  }
+  
+  
+  function wagesEarnedOnDate(date){
+  let payRate = this.payPerHour
+  //console.log(payRate)
+     return payRate * hoursWorkedOnDate.call(this, date)
+  }
+  
+  
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +81,18 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(src, firstName){
+    //console.log(JSON.stringify(firstName))
+    let findName = src.find(arr => arr.firstName === firstName)
+
+    return findName
+}
+
+function calculatePayroll(arr){
+    let grandTotal = arr.reduce((acc, current) => {
+        //console.log(current)
+       return acc + allWagesFor.call(current)
+       
+      } ,0 )
+       return grandTotal
+}
